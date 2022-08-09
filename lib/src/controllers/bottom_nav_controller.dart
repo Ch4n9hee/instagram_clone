305a8452ff49b548a3eea6d9401875a9abae9884
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2022-08-09 23:38:19 +0900

    에셋 이미지 추가, 바텀 네비게이션 바 일부 구현

---
+++ lib/src/controllers/bottom_nav_controller.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+import 'package:instagram_clone/src/pages/upload.dart';
+
+enum PageName{HOME,SEARCH,UPLOAD,ACTIVITY, MYPAGE}
+class BottomNavController extends GetxController{
+  RxInt pageIndex = 0.obs;
+
+  void changeBottomNav(int value) {
+    final page = PageName.values[value];
+    switch(page){
+      case PageName.UPLOAD:
+        Get.to(()=>Upload());
+        break;
+      case PageName.HOME:
+      case PageName.SEARCH:
+      case PageName.ACTIVITY:
+      case PageName.MYPAGE:
+        _changePage(value);
+        break;
+    }
+  }
+
+  void _changePage(int value){
+    pageIndex(value);
+  }
+}
